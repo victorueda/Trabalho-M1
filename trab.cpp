288f4ec5d735f1a8390f0866042ae9b6f4298101
GabrielAndradeU <gabrieldeandradeurbano88@gmail.com>
2024-09-08 02:43:14 -0300

    inicio

---
+++ trab.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int lerquantidade() {
+    int n;
+
+    do {
+        cout << "Digite a quantidade de cidades (Min 4 - Max 20): " << endl;
+        cin >> n;
+        if (n < 4 || n > 20) {
+            cout << "Quantidade fora do intervalo." << endl;
+            cin.clear(); // Limpa o estado de erro do cin
+            cin.ignore();
+        }
+    } while (n < 4 || n > 20);
+
+    return n;
+}
+
+int main() {
+    int qtdcidades = lerquantidade();
+    cout << qtdcidades << endl;
+
+    return 0;
+}
